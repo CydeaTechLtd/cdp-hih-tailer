1703bb3c66004ec654fa1e06de62fe560d81e892
Ijlal Haider <ijlal.haider@cydea.tech>
2021-01-22 11:31:18 +0000

    Path correction

--- sysmon_scapy_installer.ps1
+++ sysmon_scapy_installer.ps1
@@ -1,9 +1,9 @@
 #Install Python and scapy
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0.exe" -OutFile "c:/temp/python-3.7.0.exe"
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0.exe" -OutFile "c:/ProgramData/python-3.7.0.exe"
 
-c:/temp/python-3.7.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+c:/ProgramData/python-3.7.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
 pip install scapy
 #Installing SysMon
 # Variables

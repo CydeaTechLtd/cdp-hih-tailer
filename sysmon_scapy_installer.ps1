0a161303a67b582901b5cac8560bab213c8d5fa6
ijlalhaider1996 <46891614+ijlalhaider1996@users.noreply.github.com>
2021-02-02 15:29:40 +0500

    Update sysmon_scapy_installer.ps1

--- sysmon_scapy_installer.ps1
+++ sysmon_scapy_installer.ps1
@@ -4,12 +4,13 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 choco install git.install -y
 choco install nssm -y
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0-amd64.exe" -OutFile "c:/ProgramData/python-3.7.0.exe"
-refreshenv
-c:/ProgramData/python-3.7.0-amd64.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
-#c:/ProgramData/python-3.7.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-refreshenv
-pip install scapy
+#Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.0/python-3.7.0-amd64.exe" -OutFile "c:/ProgramData/python-3.7.0.exe"
+#refreshenv
+#c:/ProgramData/python-3.7.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+#c:/ProgramData/python-3.7.0.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+#refreshenv
+choco install python --version=3.7.2 -y
+
 refreshenv
 #Installing SysMon
 # Variables
@@ -74,26 +75,5 @@ if ($OSArch.osarchitecture -eq '64-bit') {
 else {
   Start-Process -FilePath "$env:SystemRoot\$sysMon32.exe" -ArgumentList "-accepteula -i $env:SystemRoot\$configFile"
 }
-cd $HOME\Downloads
-git clone https://github.com/CydeaTechLtd/cdp-hih-tailer
-pip install -r requirements.txt
-#Services
-nssm install "sysmontailor" %LOCALAPPDATA%\Python37\pythonw.exe "$HOME\Downloads\cdp-hih-tailer\Sysmon_Tailor.py"
-nssm set sysmontailor Start SERVICE_DELAYED_AUTO_START
-#nssm set sysmontailor AppDirectory "$HOME\Downloads\cdp-hih-tailer"
-nssm install "file_watcher" "C:\Windows\System32\WindowsPowerShell\v1\powershell.exe" "$HOME\Downloads\cdp-hih-tailer\file_watcher.ps1"
-nssm set file_watcher Start SERVICE_DELAYED_AUTO_START
-#nssm set file_watcehr AppDirectory "$HOME\Downloads\cdp-hih-tailer"
-nssm install "scapytailor" "%LOCALAPPDATA%\Python37\pythonw.exe" "$HOME\Downloads\cdp-hih-tailer\Scapy_Tailor.py"
-nssm set scapytailor Start SERVICE_DELAYED_AUTO_START
-#nssm set scapytailor AppDirectory "$HOME\Downloads\cdp-hih-tailer"
-nssm install "filetailor" "%LOCALAPPDATA%\Python37\pythonw.exe" "$HOME\Downloads\cdp-hih-tailer\File_Tailor.py"
-nssm set filetailor Start SERVICE_DELAYED_AUTO_START
-#nssm set filetailor AppDirectory "$HOME\Downloads\cdp-hih-tailer"
-nssm install "readfile" "%LOCALAPPDATA%\Python37\pythonw.exe" "$HOME\Downloads\cdp-hih-tailer\Read_Logs_From_File.py"#nssm set readfile AppDirectory "$HOME\Downloads\cdp-hih-tailer"
-nssm set readfile Start SERVICE_DELAYED_AUTO_START
-#nssm set readfile AppDirectory "$HOME\Downloads\cdp-hih-tailer"
-nssm install "monitor" "C:\Windows\System32\WindowsPowerShell\v1\powershell.exe" "$HOME\Downloads\cdp-hih-tailer\startup_service_monitor.ps1"
-nssm set monitor Start SERVICE_DELAYED_AUTO_START
-
- #>
+choco install winpcap -y
+pip install scapy
